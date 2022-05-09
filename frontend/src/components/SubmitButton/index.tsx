debdd3d9764fa55dc02bdf3a7ba023d4c94a6e11
Víctor Fernández <victor141516@192.168.1.135>
2022-05-09 15:13:43 +0200

    refactor: move tailwind classes to css file

--- frontend/src/components/SubmitButton/index.tsx
+++ frontend/src/components/SubmitButton/index.tsx
@@ -2,28 +2,7 @@ import styles from './index.module.css'
 
 export default function SubmitButton({ children }: { children: React.ReactNode }) {
   return (
-    <button
-      className={`
-        ${styles.submitButton}
-        inline-flex
-        items-center
-        md:px-12
-        md:py-6
-        px-8
-        py-3
-        text-2xl
-        md:text-4xl
-        font-light
-        rounded-full
-        shadow-sm
-        text-white
-        focus:outline-none
-        focus:ring-2
-        focus:ring-offset-2
-        focus:ring-primary-400
-      `}
-      type="submit"
-    >
+    <button className={styles.submitButton} type="submit">
       {children}
     </button>
   )
